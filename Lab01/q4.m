7145c5ad382da95ece4343e26cbdd9cb2fb3bf68
luk3Sky <ganindudananja@gmail.com>
2018-10-23 15:27:57 +0530

    q4 not finished

--- Lab01/q4.m
+++ Lab01/q4.m
@@ -1,14 +1,26 @@
-time_vals = [0];
 i = 0;
-for n = [500 : 500 : 5000 ],
+n = [500 : 500 : 5000];
+log_time = [];
+log_n = [];
+
+for j = n,
+  id = 0;
   id = tic();
-  A = rand(n);
+  A = rand(j);
   detA = det(A);
   t = toc(id);
   #n + = 500;
   disp(num2str(t));
-  time_vals = [time_vals, t];
+  log_n = [log_n, j];
+  log_time = [log_time, t];
   i += 1;
 endfor
 
-loglog(time_vals,n)
+#disp(num2str(log_n));
+#disp(num2str(log_time));
+t = polyfit(log_n, log_time, 2);
+disp(num2str(t));
+#hold;
+plot(log(log_n),t,'o');
+loglog(log_n, log_time);
+
