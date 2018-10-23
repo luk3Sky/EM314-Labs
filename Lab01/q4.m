585950e03b6676307ab1dd2d95d307d74ce23adc
luk3Sky <ganindudananja@gmail.com>
2018-10-23 13:52:40 +0530

    working on q4.m

--- Lab01/q4.m
+++ Lab01/q4.m
@@ -1,1 +1,14 @@
-A = rand(20)
+time_vals = [0];
+i = 0;
+for n = [500 : 500 : 5000 ],
+  id = tic();
+  A = rand(n);
+  detA = det(A);
+  t = toc(id);
+  #n + = 500;
+  disp(num2str(t));
+  time_vals = [time_vals, t];
+  i += 1;
+endfor
+
+loglog(time_vals,n)
