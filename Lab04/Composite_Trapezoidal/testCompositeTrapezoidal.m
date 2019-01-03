f5ad0d7271388bc98532f63baf2fc25c3b02e3ea
luk3Sky <ganindudananja@gmail.com>
2019-01-04 00:07:38 +0530

    Composite trapezoidal done

---
+++ Lab04/Composite_Trapezoidal/testCompositeTrapezoidal.m
@@ -0,0 +1,11 @@
+function [] = testCompositeTrapezoidal(m, n, lowerBound, upperBound, segments),
+  step = (upperBound - lowerBound)/segments;
+  f = @(x) (x^(m-1))*((1-x)^(n-1));
+  count = 0;
+  sum = 0;
+  while(count < segments),
+    sum = sum + trapezoidal(f, (lowerBound + count*step), (lowerBound + (count+1)*step), 1);
+    count++;
+  endwhile
+  disp(sum);
+endfunction
--- Lab04/Simpsons_1_3/testSimpsons_1_3.m
+++ Lab04/Simpsons_1_3/testSimpsons_1_3.m
@@ -3,4 +3,4 @@ b = 4;
 
 f = @(x) (1-x-4*(x^3)+2*(x^5));
 
-I = simpsons_1_3(f, a, b, 4);
+I = simpsons_1_3(f, a, b, 6);
