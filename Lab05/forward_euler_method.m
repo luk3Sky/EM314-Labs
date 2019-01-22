09730935a37dea8de0da791e3617ed921ab507c3
luk3Sky <ganindudananja@gmail.com>
2019-01-22 23:47:16 +0530

    Styled the printf

--- Lab05/forward_euler_method.m
+++ Lab05/forward_euler_method.m
@@ -1,8 +1,10 @@
 function y = forward_euler_method(f, y_init, upperBound, lowerBound, step),
   iter = 1;
   y(iter) = y_init;
+  printf('xn \t\tyn \t\ty_n+1\n');
   for x = lowerBound: step: upperBound - step,
     y(iter+1) = y(iter) + step*f(x, y(iter));
+    printf('%f \t%f \t%f\n', x, y(iter), y(iter+1));
     iter = iter + 1;
   endfor
   disp(y);
--- Lab05/improved_euler_method.m
+++ Lab05/improved_euler_method.m
@@ -1,7 +1,7 @@
 function y = improved_euler_method(f, y_init, upperBound, lowerBound, step),
   iter = 1;
   y(iter) = y_init;
-  printf('xn \t\tyn \t\ty_n+1_avg \ty_n+1\n')
+  printf('xn \t\tyn \t\ty_n+1_avg \ty_n+1\n');
   for x = lowerBound: step: upperBound - step,
     tempVal = y(iter) + step*f(x, y(iter));   # Predictor step
     y(iter+1) = y(iter) + step*( f(x, y(iter)) + f(x+step, tempVal) )/2;  # Corrector step
