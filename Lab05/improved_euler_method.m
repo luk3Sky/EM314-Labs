f5b92b5e46ce5b2417404abf7854fc2a446624aa
luk3Sky <ganindudananja@gmail.com>
2019-01-22 23:45:21 +0530

    Improved_euler_method done

--- Lab05/improved_euler_method.m
+++ Lab05/improved_euler_method.m
@@ -1,8 +1,11 @@
 function y = improved_euler_method(f, y_init, upperBound, lowerBound, step),
   iter = 1;
   y(iter) = y_init;
+  printf('xn \t\tyn \t\ty_n+1_avg \ty_n+1\n')
   for x = lowerBound: step: upperBound - step,
-    y(iter+1) = y(iter) + step*f(x, y(iter));
+    tempVal = y(iter) + step*f(x, y(iter));   # Predictor step
+    y(iter+1) = y(iter) + step*( f(x, y(iter)) + f(x+step, tempVal) )/2;  # Corrector step
+    printf('%f \t%f \t%f \t%f\n', x, y(iter), tempVal, y(iter+1));
     iter = iter + 1;
   endfor
   disp(y);
